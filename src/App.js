e1ba6f04f305aa080117fa0cc1a5b33e7b3f31b9
Charlie Cureton <ccureton21@gmail.com>
2022-08-02 20:26:26 +0100

    Final Product

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 
 function App() {
   return (
     <div className="App">
       <h1>Material UI with React.JS</h1>
+      <Button variant="contained" style={{
+        backgroundColor: '#FF005D',
+        padding: '10px'
+      }}>Click Me!</Button>
+      <AddIcon />
+
     </div>
   );
 }
